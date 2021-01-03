d9477f5d47a842f53265ceabeee714dd52626be9
jiro4989 <jiroron666@gmail.com>
2021-01-03 16:42:25 +0900

    chore: add runnable examples

--- src/json2codepkg/util.nim
+++ src/json2codepkg/util.nim
@@ -1,4 +1,12 @@
 import strutils
 
 func headUpper*(text: string): string =
+  ## Returns upper camel case (only prefix 1 character).
+  ## Returns an empty string when `text` is an empty string.
+  runnableExamples:
+    doAssert "hello".headUpper == "Hello"
+    doAssert "h".headUpper == "H"
+    doAssert "".headUpper == ""
+
+  if text.len < 1: return ""
   return text[0].toUpperAscii & text[1..^1]
