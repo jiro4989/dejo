ca40d6c0bf4377276f9213773205040f50caf7f6
jiro4989 <jiroron666@gmail.com>
2021-01-03 16:33:15 +0900

    chore: add test cases

---
+++ tests/test_generator_nim.nim
@@ -0,0 +1,34 @@
+import unittest
+
+include json2codepkg/[parser, generator_nim]
+
+suite "proc generateNimCode":
+  setup:
+    var node: JsonNode
+    let objName = "Obj"
+
+  test "ok: generate simple json":
+    node = """{"strval":"hello","intval":1,"floatval":1.2,"boolval":true}""".parseJson
+    let got = node.parse(objName = objName).generateNimCode
+    let want = """type
+  Obj* = ref object
+    strval*: string
+    intval*: int64
+    floatval*: float64
+    boolval*: bool"""
+    check want == got
+
+  test "ok: nested json":
+    node = """{"obj1":{"s":"hello","n":1},"obj2":{"f":1.2}}""".parseJson
+    let got = node.parse(objName = objName).generateNimCode
+    let want = """type
+  Obj* = ref object
+    obj1*: Obj1
+    obj2*: Obj2
+  Obj1* = ref object
+    s*: string
+    n*: int64
+  Obj2* = ref object
+    f*: float64"""
+    check want == got
+
